19a3689cdc3ab3a65bdc4453ffe434590da5a8c5
Claude Cochet <code@main2.fr>
2016-03-03 08:19:12 +0100

    iterating on books/sheets/formulas

--- src/excel_com.js
+++ src/excel_com.js
@@ -28,6 +28,7 @@ function test() {
 var folder="..\\..\\..\\vision\\Bin\\xls";
 //C:\Code\vision\Bin\xls";
 
+var path = require('path');
 var fs = require('fs');
 var win32ole = require('win32ole');
 var xl = win32ole.client.Dispatch('Excel.Application');
@@ -37,17 +38,63 @@ fs.readdir(folder, function(err, files) {
 	if (err) throw err;
 	console.log(files);
 	for (var f=0; f<files.length; f++) {
-		var path = folder+"\\"+files[f];
+		var filepath = path.resolve(folder+"\\"+files[f]);
 		//fs.stat(), fs.lstat() and fs.fstat() and their synchronous counterparts are of this type.
 		//stats.isDirectory()
-		if (path.indexOf(".xls")!=path.length-4)
+		if (filepath.indexOf(".xls")!=filepath.length-4)
 			continue;
 		
-		var book = xl.Workbooks.Open(path);
-		var nb_sheet = book.Worksheets.length;
-		
-		console.log(path);
-		console.log(nb_sheet);
+		console.log("filepath: "+filepath);
+		var book = xl.Workbooks.Open(filepath);
+		var nb_sheet = book.Worksheets.Count;
+		console.log("nb_sheet: "+nb_sheet);
+		for (var s=0; s<nb_sheet; s++) {
+			console.log("sheet: "+(s+1)+"/"+nb_sheet);
+			var sheet = book.Worksheets(s+1);
+			console.log("sheet: "+sheet.Name);
+			// Range("B4").End(xlUp).Select  go to end of contiguous range
+			
+			console.log(""+sheet.Cells.Rows.Count);
+			var xlCellTypeLastCell =11;
+			var rng_bottom_right = sheet.Cells(1,1) .SpecialCells(xlCellTypeLastCell)
+			var n = rng_bottom_right.Row, 
+				m= rng_bottom_right.Column;
+			console.log(n+" x "+m);
+			//
+			for (var i=0; i<n; i++) {
+				for (var j=0; j<m; j++) {
+					console.log(i+" x "+j+" "+sheet.Cells(i+1,j+1).FormulaR1C1);
+				}
+			}
+			
+			//
+		}
 	
+		book.Close();
+		break;
 	}
 });
+
+/*
+var win32com = require('win32com');
+var xl = win32com.client.Dispatch('Excel.Application', 'C'); // locale 
+xl.Visible = true;
+var book = xl.Workbooks.Add();
+var sheet = book.Worksheets(1);
+sheet.Name = 'sheetnameA utf8';
+sheet.Cells(1, 2).Value = 'test utf8';
+var rg = sheet.Range(sheet.Cells(2, 2), sheet.Cells(4, 4));
+rg.RowHeight = 5.18;
+rg.ColumnWidth = 0.58;
+rg.Interior.ColorIndex = 6; // Yellow 
+book.SaveAs('testfileutf8.xls');
+xl.ScreenUpdating = true;
+xl.Workbooks.Close();
+xl.Quit();
+
+Application.ScreenUpdating = False
+        Cancel = True
+        LastRow = Cells(Rows.Count, keyColumn).End(xlUp).Row
+        Set SortRange = Target.CurrentRegion
+        
+*/
