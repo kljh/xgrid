159a33a2e5832b61683708556157f9e9c8a0d5a7
Claude Cochet <code@main2.fr>
2016-03-09 06:39:56 +0000

    Excel Range Parsing - A1 style: columns name to pos - R1c1 style: fixed regexp - handled named range

--- src/excel_range_parse.js
+++ src/excel_range_parse.js
@@ -3,7 +3,8 @@ function parse_range_test() {
 	var ranges = [
 		'$A1',
 		'$B$2',
-		'Sheet1!A1',
+		'AA$3',
+		'Sheet1!Z1',
 		'\'Another sheet\'!A1',
 		'[Book1]Sheet1!$A$1',
 		'[data.xls]sheet1!$A$1',
@@ -24,6 +25,7 @@ function parse_range_test() {
 		'R13C3',
 		'R[4]C10',
 		'R23C[11]',
+		'named_cell'
 		];
 	
 	for (var r=0; r<ranges.length; r++) {
@@ -32,38 +34,9 @@ function parse_range_test() {
 	}
 }
 
-function parse_cell(cell) {
-	if (!cell) return cell;
-	
-	var r1c1 = /(R(\[?([0-9]*)\]))?(C(\[?([0-9]*)\]))?/.exec(cell);
-	info_msg(JSON.stringify(r1c1,null,4));
-	if (r1c1[3] || r1c1[6] ) {
-		return { 
-			row : r1c1[3],
-			row_abs : r1c1[2]==r1c1[3],
-			col : r1c1[6],
-			col_abs : r1c1[5]==r1c1[6],
-			style : "R1C1"
-		};
-	}
-	//
-	var a1 = /(\$?([A-Z]*))?(\$?([0-9]*))?/.exec(cell);
-	if (a1[2] || a1[4] ) {
-		return { 
-			row : a1[2],
-			row_abs : a1[1]!=a1[2],
-			col : a1[4],
-			col_abs : a1[3]!=a1[4],
-		};
-	}
-}
-	
 function parse_range(arg) {
 	
 	var rex = /((.*!)?)([^!]*)/.exec(arg);
-	if (rex.length!=4) {
-		return { name : arg };
-	} 
 	var book_and_sheet = rex[2];
 	var range = rex[3];
 	var cells = range.split(":");
@@ -80,9 +53,12 @@ function parse_range(arg) {
 		var sheet = rex[3];
 	}
 	
+	// named cell
+	if (!cell0)
+		return arg;
+	
 	return { 
 		arg : arg,
-		range: range, 
 		cell0 : cell0,
 		cell1 : cell1,
 		sheet : sheet ? sheet.replace(/''/g, '\'') : sheet,
@@ -92,8 +68,51 @@ function parse_range(arg) {
 	
 }
 
+function parse_cell(cell) {
+	if (!cell) return cell;
+	
+	var r1c1 = /(R(\[?(-?[0-9]*)\]?))?(C(\[?(-?[0-9]*)\]?))?/.exec(cell);
+	if (r1c1[3] || r1c1[6] ) {
+		return { 
+			row : r1c1[3],
+			row_abs : r1c1[2]==r1c1[3],
+			col : r1c1[6],
+			col_abs : r1c1[5]==r1c1[6],
+			//style : "R1C1"
+		};
+	}
+	//
+	var a1 = /(\$?([A-Z]*))?(\$?([0-9]*))?/.exec(cell);
+	if (a1[2] || a1[4] ) {
+		return { 
+			col : parse_column_name_to_index(a1[2]),
+			col_abs : a1[1]!=a1[2],
+			row : parseFloat(a1[4]),
+			row_abs : a1[3]!=a1[4],
+		};
+	}
+}
+
+function parse_column_name_to_index(col) {
+	if (!col) return col;
+
+	var pos = 0;
+	var mult = 1;
+	var A = col.charCodeAt(0);
+	var Z = col.charCodeAt(0);
+	for (var i=col.length-1; i>=0; i--) {
+		var v = col.charCodeAt(i) - A + 1;
+		pos += mult*v;
+		mult *= 26;
+	}
+	return pos;
+}
+
 function info_msg(msg) {
-	console.log(msg);
+	if (typeof console != "undefined")
+		console.log(msg);
+	else 
+		print(msg);
 }
 
 parse_range_test();
