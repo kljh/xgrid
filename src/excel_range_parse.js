6fe34d012410445dea715ee72f641e29f3150e1c
charles90 <sicat.charles@gmail.com>
2016-04-11 23:03:07 +0100

    Fix to parse_range for R[1] (mode arg not necessary ideally) Fix to stringify_range used as key for the dependency graph (issue with offset of char code of parse_column_index_to_name) Added and updated respective tests

--- src/excel_range_parse.js
+++ src/excel_range_parse.js
@@ -37,23 +37,31 @@ function parse_range_test() {
 
 function range_regression_tests() {
 	assert.equal(JSON.stringify(parse_range('R1')),
-		'{"arg":"R1","cell0":{"col":18,"col_abs":false,"row":1,"row_abs":false}}');
+		'{"col":18,"abs_col":false,"row":1,"abs_row":false}');
 	assert.equal(JSON.stringify(parse_range('R[1]')),
-		'{"arg":"R[1]","cell0":{"row":"1","row_abs":false,"col_abs":true}}');
+		'{"row":"1","abs_row":false,"abs_col":true}');
 	assert.equal(JSON.stringify(parse_range('R[1]C[1]')),
-		'{"arg":"R[1]C[1]","cell0":{"row":"1","row_abs":false,"col":"1","col_abs":false}}');
+		'{"row":"1","abs_row":false,"col":"1","abs_col":false}');
+	assert.equal(JSON.stringify(parse_range('AA5')),
+		'{"col":27,"abs_col":false,"row":5,"abs_row":false}');
+	
+	var aa5 = parse_range("AA5");
+	assert.equal(stringify_range(aa5), 'AA5');
 }
 
-function parse_range(arg) {
-	
+function parse_range(arg, mode) {
+
 	var rex = /((.*!)?)([^!]*)/.exec(arg);
 	var book_and_sheet = rex[2];
 	var range = rex[3];
+
 	var cells = range.split(":");
 	if (cells>2) throw new Error(arguments.callee.name+": "+arg)
-	var cell0 = parse_cell(cells[0]);
-	var cell1 = parse_cell(cells[1]);
-	
+	var cell0 = parse_cell(cells[0], mode);
+	var cell1 = parse_cell(cells[1], mode);
+	// named cell
+	cell0 = cell0 || cells[0];
+
 	if (book_and_sheet) {
 		var quoted = (book_and_sheet[0]=='\'');
 		var len = book_and_sheet.length;
@@ -61,44 +69,40 @@ function parse_range(arg) {
 		var rex = /(\[(.*)\])?(.*)/.exec(book_and_sheet);
 		var book = rex[2];
 		var sheet = rex[3];
+
+		book = book ? book.replace(/''/g, '\'') : book;
+		sheet = sheet ? sheet.replace(/''/g, '\'') : sheet;
 	}
-	
-	// named cell
-	if (!cell0)
-		return arg;
-	
-	return { 
-		arg : arg,
-		cell0 : cell0,
-		cell1 : cell1,
-		sheet : sheet ? sheet.replace(/''/g, '\'') : sheet,
-		book : book ? book.replace(/''/g, '\'') : book,
-		 };
-	//info_msg(rex.length + " " + JSON.stringify(rex));
-	
+
+	var res = cell0;
+	res.end = cell1;
+	res.book = book;
+	res.sheet = sheet;
+
+	return res;
 }
 
-function parse_cell(cell) {
+function parse_cell(cell, mode) {
 	if (!cell) return cell;
-	
+
 	var r1c1 = /(R(\[(-?[0-9]*)\]))?(C(\[(-?[0-9]*)\]))?/.exec(cell);
-	if (r1c1[3] || r1c1[6] ) {
+	if ( (r1c1[3] || r1c1[6]) || mode=="R1C1" ) {
 		return { 
 			row : r1c1[3],
-			row_abs : r1c1[2]==r1c1[3],
+			abs_row : r1c1[2]==r1c1[3],
 			col : r1c1[6],
-			col_abs : r1c1[5]==r1c1[6],
+			abs_col : r1c1[5]==r1c1[6],
 			//style : "R1C1"
 		};
 	}
 	//
 	var a1 = /(\$?([A-Z]*))?(\$?([0-9]*))?/.exec(cell);
-	if (a1[2] || a1[4] ) {
+	if ( (a1[2] && a1[4]) || mode=="A1" ) {
 		return { 
 			col : parse_column_name_to_index(a1[2]),
-			col_abs : a1[1]!=a1[2],
+			abs_col : a1[1]!=a1[2],
 			row : parseFloat(a1[4]),
-			row_abs : a1[3]!=a1[4],
+			abs_row : a1[3]!=a1[4],
 		};
 	}
 }
@@ -108,13 +112,37 @@ function parse_column_name_to_index(col) {
 
 	var pos = 0;
 	var mult = 1;
-	var valOfA = "A".charCodeAt(0);
+	var A = "A".charCodeAt(0);
 	for (var i=col.length-1; i>=0; i--) {
-		var v = col.charCodeAt(i) - valOfA + 1;
+		var v = col.charCodeAt(i) - A + 1;
 		pos += mult*v;
 		mult *= 26;
 	}
 	return pos;
+}
+
+function stringify_range(rng, mode) {
+	if (rng.sheet || rng.book)
+		throw new Error(arguments.callee.name+": external ranges not supported");
+
+	return (rng.abs_col ? "$" : "") + parse_column_index_to_name(rng.col)
+		+ (rng.abs_row ? "$" : "") + rng.row
+		+ (rng.end ? ":"+stringify_range(rng.end, mode) : ""); 
+	
+	return "R"+(rng.abs_row ? rng.row : "["+rng.row+"]") 
+		+ "C"+(rng.abs_col ? rng.col : "["+rng.col+"]")
+		+ (rng.end ? ":"+stringify_range(rng.end, mode) : ""); 
+}
+
+function parse_column_index_to_name(pos) {
+	var col = "";
+	var A = "A".charCodeAt(0);
+	do {
+		var u = pos%26;
+		var pos = (pos-u)/26;
+		col = String.fromCharCode(A+u-1) + col;
+	} while (pos!=0)
+	return col;
 }
 
 function info_msg(msg) {
@@ -128,11 +156,20 @@ function dbg(name, variable) {
 	info_msg(name+":\n"+JSON.stringify(variable,null,4));
 }
 
+// Nodejs stuff
+if (typeof module!="undefined") {
 
-if (module == require.main) {
-	range_regression_tests();
+	var parser = require("./excel_formula_parse")
+
+	// run tests if this file is called directly
+	if (require.main === module) {
+		parse_range_test();
+		range_regression_tests();
+	}
+
+	module.exports.parse_range = parse_range;
+	module.exports.stringify_range = stringify_range;
+	module.exports.info_msg = info_msg;
+	module.exports.dbg = dbg;
+
 }
-
-// parse_range_test();
-
-module.exports.parse_range = parse_range;
